4c7428edb5846e8295bab30115ddd6aa2ab0991a
Diana <63928714+Ciklop-Ciklop@users.noreply.github.com>
2020-04-23 18:18:32 +0300

    Add files via upload

---
+++ src/src/Student_Diana_Miranovica/HelloWorld.java
@@ -0,0 +1,16 @@
+package Student_Diana_Miranovica;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+
+    }
+}
